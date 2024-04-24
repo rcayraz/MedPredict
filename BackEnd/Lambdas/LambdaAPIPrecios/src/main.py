ebc6d515571bbc75f2f0e728d8353a24be3d978e
rcayraz <ayra.zevallos@gmail.com>
2024-04-23 22:50:44 -0500

    add lambda process get precios vs

---
+++ BackEnd/Lambdas/LambdaAPIPrecios/src/main.py
@@ -0,0 +1,24 @@
+import json
+import pandas as pd
+import pyarrow.parquet as pq
+from s3fs import S3FileSystem
+
+def lambda_handler(event, context):
+    s3 = S3FileSystem(anon=False)
+    bucket_name = "nombre-de-tu-bucket"
+    file_key = "ruta/a/tu/archivo.parquet"
+    file_path = f's3://{bucket_name}/{file_key}'
+    df = pd.read_parquet(file_path, filesystem=s3)
+    try:
+        body = event.get('body',None)
+        if body is None:
+            return {"statusCode":400 , "body":"requiere el body en el request"}
+        descripcion = json.loads(body)
+        descripcion = descripcion.get('Descripcion')
+        filtered_df = df[df['descripcion_columna'].str.contains(descripcion)]
+        result = filtered_df.to_json(orient='records')
+        return {"statusCode": 200, "body": json.dumps(result)}
+    except KeyError as e:
+        return e
+        
+
